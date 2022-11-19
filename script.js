c3f0d03be5b9ce65c522c2a0c36291f337580622
Loadigjablos <loadingjablowme@gmail.com>
2022-11-19 20:58:46 +0100

    Deleted Errors

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ var conversation = document.querySelector("#Conversation");
  */
 document.getElementById("send-text").addEventListener('click', function(event) {
     requestOne = new XMLHttpRequest();
-    //requestOne.open("GET", "http://localhost/API/other/Value/" + textInput.value);
-    requestOne.open("GET", "http://localhost/API/other");
+    requestOne.open("GET", "API/v1/Value/" + textInput.value);
     requestOne.onreadystatechange = onRequstUpdate;
-
     requestOne.send();
     onRequstUpdate(event);
 });
 
 /**
- * here will be the validation of the result
+ * here will be the validation of the result and it paste it in the website
  * @param {*} event nothing
- * @returns if the server didn't responde corectly
+ * @returns if the server didn't responde or connect corectly
  */
 function onRequstUpdate(event) {
     if (requestOne.readyState < 4) {
         return;
     }
-    conversation.innerText += textInput.value + "\n";
-
-    console.log(requestOne.responseText);
+    conversation.innerText += "YOU:\t\t" + textInput.value + "\n";
 
     var result = JSON.parse(requestOne.responseText);
 
-    conversation.innerText += result.output_value + "\n";
+    if (result.output_value == undefined || result.output_value == null) {
+        conversation.innerText += "AI:\t\t*Silence*\n";
+    } else {
+        conversation.innerText += "AI: \t\t" + result.output_value + "\n";
+    }
 }
